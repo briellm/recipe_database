60dcf18f0a99a145d1f552f3aaa8972908b7d407
briellm <100302084+briellm@users.noreply.github.com>
2025-12-01 14:41:21 -0500

    made some updates to make things match

--- recipe-database.sql
+++ recipe-database.sql
@@ -1,15 +1,17 @@
-INSERT INTO Continent (Continent, Country) VALUES ('Asia', 'Japan');
-INSERT INTO Continent (Continent, Country) VALUES ('Europe', 'Italy');
-INSERT INTO Continent (Continent, Country) VALUES ('North America', 'USA');
+INSERT INTO Continent (Continent_ID, Continent, Country) VALUES (1, 'Asia', 'Japan');
+INSERT INTO Continent (Continent_ID, Continent, Country) VALUES (2, 'Europe', 'Italy');
+INSERT INTO Continent (Continent_ID, Continent, Country) VALUES (3, 'North America', 'USA');
+
 
 INSERT INTO Common_Allergies (Allergy_Type, Allergy_ID) VALUES ('Gluten', 1);
 INSERT INTO Common_Allergies (Allergy_Type, Allergy_ID) VALUES ('Peanuts', 2);
 INSERT INTO Common_Allergies (Allergy_Type, Allergy_ID) VALUES ('Dairy', 3);
 INSERT INTO Common_Allergies (Allergy_Type, Allergy_ID) VALUES ('Shellfish', 4);
 
-INSERT INTO Cuisine (Cuisine_Type, Continent) VALUES ('Italian', 'Europe');
-INSERT INTO Cuisine (Cuisine_Type, Continent) VALUES ('Japanese', 'Asia');
-INSERT INTO Cuisine (Cuisine_Type, Continent) VALUES ('American', 'North America');
+INSERT INTO Cuisine (Cuisine_ID, Cuisine_Type, Continent_ID) VALUES (1, 'Italian', 2);
+INSERT INTO Cuisine (Cuisine_ID, Cuisine_Type, Continent_ID) VALUES (2, 'Japanese', 1);
+INSERT INTO Cuisine (Cuisine_ID, Cuisine_Type, Continent_ID) VALUES (3, 'American', 3);
+
 
 INSERT INTO Recipe (
     Recipe_ID, Recipe_Name, Cook_Time, Meal_Type, Spice_Level,
@@ -23,20 +25,35 @@ INSERT INTO Recipe (
 
 INSERT INTO Recipe (
     Recipe_ID, Recipe_Name, Cook_Time, Meal_Type, Spice_Level,
-    Difficulty_Level, Cook_Type, Cuisine_Type,
-    Ingredient_ID, Allergy, Required_Utensil
+    Difficulty_Level, Cook_Type, Cuisine_Type
 ) VALUES (
     102, 'Peanut Stir Fry', 15, 'Lunch', 3,
-    3, 'Stir-Fry', 'Japanese',
-    2, 2, 1
+    3, 'Stir-Fry', 'Japanese'
+    
 );
 
 INSERT INTO Recipe (
     Recipe_ID, Recipe_Name, Cook_Time, Meal_Type, Spice_Level,
-    Difficulty_Level, Cook_Type, Cuisine_Type,
-    Ingredient_ID, Allergy, Required_Utensil
+    Difficulty_Level, Cook_Type, Cuisine_Type
+    
 ) VALUES (
     103, 'Milk Pancakes', 10, 'Breakfast', 0,
-    1, 'Pan-Fry', 'American',
-    3, 3, 3
+    1, 'Pan-Fry', 'American'
+   
 );
+
+//Spaghetti with Shrimp
+INSERT INTO Recipe_Ingredients VALUES (101, 4, 1, 'lb');
+INSERT INTO Recipe_Allergies VALUES (101, 4);
+INSERT INTO Recipe_Utensils VALUES (101, 2);
+
+//Peanut Stir Fry
+INSERT INTO Recipe_Ingredients VALUES (102, 2, 0.5, 'cup');
+INSERT INTO Recipe_Allergies VALUES (102, 2);
+INSERT INTO Recipe_Utensils VALUES (102, 1);
+
+//milk pancakes
+INSERT INTO Recipe_Ingredients VALUES (103, 3, 1, 'cup');
+INSERT INTO Recipe_Allergies VALUES (103, 3);
+INSERT INTO Recipe_Utensils VALUES (103, 3);
+
